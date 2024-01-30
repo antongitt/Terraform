510b88f64a0a47cc27a6a44307c33a2620bccf0f
antongitt <91033128+antongitt@users.noreply.github.com>
2024-01-30 18:05:06 -0500

    Update main.tf

--- aws/backend/main.tf
+++ aws/backend/main.tf
@@ -16,11 +16,6 @@ resource "aws_s3_bucket" "terraform_state" {
   bucket = "${var.s3_bucket_name}-${data.aws_caller_identity.current.account_id}-${var.aws_region}"
 }
 
-resource "aws_s3_bucket_acl" "terraform_state_acl" {
-  bucket = aws_s3_bucket.terraform_state.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
