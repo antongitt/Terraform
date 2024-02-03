b8147f2b6c48f13f9bee1782e4310df9aabf468c
Anton Pestov <anton.pestov@gmail.com>
2024-02-02 22:45:47 -0500

    update

--- aws/mario/main.tf
+++ aws/mario/main.tf
@@ -10,15 +10,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Filter out local zones, which are not currently supported with managed node groups
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
-# Creating the Amazon EKS cluster role
+# Creating the Amazon EKS cluster role https://docs.aws.amazon.com/eks/latest/userguide/service_IAM_role.html#create-service-role
 resource "aws_iam_role" "cluster" {
   name               = "eksClusterRole"
 
@@ -47,33 +39,28 @@ data "aws_vpc" "default" {
   default = true
 }
 
-# Only use subnets from available zones
-data "aws_subnet" "available" {
-  count  = length(data.aws_availability_zones.available.names)
-  vpc_id = data.aws_vpc.default.id
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
-# Flatten the list of subnet IDs
-locals {
-  flattened_subnets = flatten([for subnet in data.aws_subnet.available : subnet.id])
-}
-
-# Create a cluster using only available subnets
+# Create a cluster
 resource "aws_eks_cluster" "cluster" {
   name     = "mario-cluster"
   role_arn = aws_iam_role.cluster.arn
 
   vpc_config {
-    subnet_ids = local.flattened_subnets
+    subnet_ids = data.aws_subnets.public.ids
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [aws_iam_role_policy_attachment.cluster]
+  depends_on = [ aws_iam_role_policy_attachment.cluster]
 }
 
-# Create the node IAM role
+# Create the node IAM role https://docs.aws.amazon.com/eks/latest/userguide/create-node-role.html
 resource "aws_iam_role" "node" {
   name               = "eksNodeRole"
 
@@ -110,12 +97,13 @@ resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   role       = aws_iam_role.node.name
 }
 
+
 # create node group
 resource "aws_eks_node_group" "group" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = "mario-group"
   node_role_arn   = aws_iam_role.node.arn
-  subnet_ids      = local.flattened_subnets
+  subnet_ids      = data.aws_subnets.public.ids
 
   scaling_config {
     desired_size = 1
@@ -132,13 +120,4 @@ resource "aws_eks_node_group" "group" {
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
-}
-
-# Output as environment variables
-output "eks_cluster_env_variables" {
-  value = {
-    EKS_CLUSTER_NAME         = aws_eks_cluster.cluster.name
-    EKS_CLUSTER_KUBE_VERSION = aws_eks_cluster.cluster.version
-    AWS_REGION               = var.aws_region
-  }
 }
--- aws/mario/script.sh
+++ aws/mario/script.sh
@@ -8,15 +8,16 @@ sudo yum -y install terraform
 # Verify the installation
 terraform version
 
+
 # Set variables
 echo 'project_name = "mario"' > ../backend/terraform.tfvars
 echo 'aws_region   = "us-east-1"' >> ../backend/terraform.tfvars
 
-echo "Creating a S3 backend with Terraform"
-cd ../backend
-echo "$PWD"
-terraform init
-terraform apply -auto-approve
+#echo "Creating a S3 backend with Terraform"
+#cd ../backend
+#echo "$PWD"
+#terraform init
+#terraform apply -auto-approve
 
 echo "Creating EKS with Terraform"
 cd ../mario
@@ -31,8 +32,9 @@ sudo yum install -y kubectl
 kubectl version --client
 
 # Update the configuration to communicate with a particular cluster
-aws eks update-kubeconfig --name ${EKS_CLUSTER_NAME} --region ${AWS_REGION}
+aws eks --region $(terraform output -raw aws_region) update-kubeconfig --name $(terraform output -raw cluster_name)
 
+# You can now use kubectl to manage your cluster and deploy Kubernetes configurations to it.
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
 kubectl get all
