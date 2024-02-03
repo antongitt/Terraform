b8393edc742987e36f2171c533fbb8912462da5d
Anton Pestov <anton.pestov@gmail.com>
2024-02-02 23:37:41 -0500

    region

--- aws/mario/script.sh
+++ aws/mario/script.sh
@@ -32,7 +32,7 @@ sudo yum install -y kubectl
 kubectl version --client
 
 # Update the configuration to communicate with a particular cluster
-aws eks --region $(terraform output -raw aws_region) update-kubeconfig --name $(terraform output -raw cluster_name)
+aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name)
 
 # You can now use kubectl to manage your cluster and deploy Kubernetes configurations to it.
 kubectl apply -f deployment.yaml
