07b4e354163309541a33ba41832c43c2471731f0
Anton Pestov <anton.pestov@gmail.com>
2024-02-05 22:45:29 -0500

    update

--- aws/mario/main.tf
+++ aws/mario/main.tf
@@ -116,7 +116,7 @@ resource "aws_eks_node_group" "group" {
     min_size     = 1
   }
 
-  instance_types = ["t2.medium"]
+  instance_types = ["t2.medium"] # .micro instance gives an error
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
