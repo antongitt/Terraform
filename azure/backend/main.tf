96628bcba264bb704dc130f6edec1d537a8fae06
Anton Pestov <anton.pestov@gmail.com>
2024-02-06 19:46:12 -0500

    azurerm_storage_container

--- azure/backend/main.tf
+++ azure/backend/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_storage_account" "sa" {
 }
 
 resource "azurerm_storage_container" "sc" {
-  name                  = "terraform"
+  name                  = var.project
   storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
 }
