ef7bea58a48633b4f1672b19467e7aa792acae69
Anton Pestov <anton.pestov@gmail.com>
2024-07-10 13:53:23 -0400

    policy/AmazonEC2FullAccess

--- aws/github-actions.sh
+++ aws/github-actions.sh
@@ -1,8 +1,13 @@
+# Define variables
 GITHUB_REPO="antongitt/Terraform"
 
+# Create OIDC provider
 aws iam create-open-id-connect-provider --url "https://token.actions.githubusercontent.com" --client-id-list "sts.amazonaws.com"
+
+# Retrieve OIDC provider ARN
 OIDC_PROVIDER_ARN=$(aws iam list-open-id-connect-providers | jq -r '.OpenIDConnectProviderList[] | select(.Arn | contains("token.actions.githubusercontent.com")) | .Arn')
 
+# Create IAM role with scoped trust policy
 aws iam create-role --role-name GitHubActionsRole --assume-role-policy-document '{
   "Version": "2012-10-17",
   "Statement": [
@@ -22,8 +27,9 @@ aws iam create-role --role-name GitHubActionsRole --assume-role-policy-document 
   ]
 }'
 
-
+# Attach policies to the role
 aws iam attach-role-policy --role-name GitHubActionsRole --policy-arn arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess
 aws iam attach-role-policy --role-name GitHubActionsRole --policy-arn arn:aws:iam::aws:policy/AmazonEKSClusterPolicy
 aws iam attach-role-policy --role-name GitHubActionsRole --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
 aws iam attach-role-policy --role-name GitHubActionsRole --policy-arn arn:aws:iam::aws:policy/IAMFullAccess
+aws iam attach-role-policy --role-name GitHubActionsRole --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess
